a611a4594c5a2941332c2caaf28f68ab53386cbb
Will Mobley <feulmob+github@gmail.com>
2025-07-23 16:05:40 -0500

    Update raingauge.py

--- raingauge.py
+++ raingauge.py
@@ -1,26 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-RAIN_PIN = 17  # or whatever GPIO pin you're using
-
-rain_count = 0
+RAIN_PIN = 18  # BCM numbering
 
 def rain_event(channel):
-    global rain_count
-    rain_count += 1
-    print(f"Raindrop detected! Total: {rain_count}")
+    print("Raindrop detected!")
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RAIN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Detect falling edge (tip of the bucket)
-GPIO.add_event_detect(RAIN_PIN, GPIO.FALLING, callback=rain_event, bouncetime=300)
-
-print("Rain gauge monitoring started. Press Ctrl+C to stop.")
 try:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)  # Make sure you're using BCM if pin 17
+    GPIO.setup(RAIN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    # Now add event detection
+    GPIO.add_event_detect(RAIN_PIN, GPIO.FALLING, callback=rain_event, bouncetime=300)
+
+    print("Rain gauge monitoring started. Press Ctrl+C to stop.")
     while True:
         time.sleep(60)
-        print(f"Rainfall total: {rain_count * 0.2794} mm")
+
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
