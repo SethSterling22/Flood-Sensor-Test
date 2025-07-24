83882f7e6cdf80e32c44e85913bb2d2927353c67
Will Mobley <feulmob+github@gmail.com>
2025-07-24 10:33:29 -0500

    Update raingauge.py

--- raingauge.py
+++ raingauge.py
@@ -5,20 +5,20 @@ RAIN_PIN = 18  # BCM numbering
 
 def rain_event(channel):
     print("Raindrop detected!")
+while True:
+    try:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)  # Make sure you're using BCM if pin 17
+        GPIO.setup(RAIN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+        # Now add event detection
+        GPIO.add_event_detect(RAIN_PIN, GPIO.FALLING, callback=rain_event, bouncetime=300)
 
-try:
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)  # Make sure you're using BCM if pin 17
-    GPIO.setup(RAIN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    
-    # Now add event detection
-    GPIO.add_event_detect(RAIN_PIN, GPIO.FALLING, callback=rain_event, bouncetime=300)
+        print("Rain gauge monitoring started. Press Ctrl+C to stop.")
+        while True:
+            time.sleep(60)
 
-    print("Rain gauge monitoring started. Press Ctrl+C to stop.")
-    while True:
-        time.sleep(60)
-
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    GPIO.cleanup()
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        GPIO.cleanup()
