4d69900471635af8fee89d569ca3eb975281a2d3
SethSterling22 <xeb03sterling@hotmail.com>
2025-09-23 17:26:21 +0000

    sync

--- Sensor-Tests/rainfall-S_Test.py
+++ Sensor-Tests/rainfall-S_Test.py
@@ -1,14 +1,24 @@
 from gpiozero import Button
 
+# GPIO number of the Rainfall Sensor
 rain_sensor = Button(6)
 count = 0
 
+# Add one to the counter when the Sensor detect a drop
 def bucket_tipped():
     global count
     count +=1
     print(count)
 
-while True:
-    rain_sensor.when_pressed = bucket_tipped
+try:
+    print("Testing liquid sensor. Press Ctrl+C to exit.")
+    
+    while True:
+        rain_sensor.when_pressed = bucket_tipped
 
+except KeyboardInterrupt:
+    print("Exiting program.")
 
+finally:
+    # Clean up GPIO settings on exit
+    GPIO.cleanup()
